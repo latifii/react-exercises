45bb4d3581984d15ba731a47258f570937fa7f1c
latifii <latifii68@gmail.com>
2023-01-09 11:46:54 +0330

    done project

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -4,7 +4,46 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#c56cf0').all(10))
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      let colors = new Values(color).all(10)
+      setError(false)
+      setList(colors)
+      console.log(colors)
+    } catch (error) {
+      console.log(error)
+      setError(true)
+    }
+  }
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button type='submit' className='btn'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((item, index) => {
+          return <SingleColor {...item} key={index} hexColor={item.hex} />
+        })}
+      </section>
+    </>
+  )
 }
 
 export default App
--- 09-color-generator/src/SingleColor.js
+++ 09-color-generator/src/SingleColor.js
@@ -1,8 +1,33 @@
 import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight, type, hexColor }) => {
+  const [alert, setAlert] = useState(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 2000)
+    return () => clearTimeout(timeout)
+  }, [alert])
+
+  const copyClipBoard = () => {
+    setAlert(true)
+    navigator.clipboard.writeText(hexValue)
+  }
+
+  const bcg = rgb.join(',')
+  const hexValue = `#${hexColor}`
+  return (
+    <article
+      className={`${type === 'shade' ? 'color-light' : ''} color `}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={copyClipBoard}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>copy to clipboard</p>}
+    </article>
+  )
 }
 
 export default SingleColor
