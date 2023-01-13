d4be5269d7fef6fa4ba1a0bbd64fdf446a3b2bf9
latifii <latifii68@gmail.com>
2023-01-13 10:11:47 +0330

    project rename

--- 14-cocktails/src/context.js
+++ 14-cocktails/src/context.js
@@ -5,9 +5,9 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  return <AppContext.Provider value='test'>{children}</AppContext.Provider>
 }
-// make sure use
+
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
