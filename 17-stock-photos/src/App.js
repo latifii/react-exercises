94a4b3c2ce211cbb37783b0c1c4f9f37ebb0c99e
latifii <latifii68@gmail.com>
2023-01-14 19:08:57 +0330

    complate photo component

--- 17-stock-photos/src/App.js
+++ 17-stock-photos/src/App.js
@@ -1,12 +1,59 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+// const Access Key = `Odj3gH_qVK7ArX7Ta_SvWt_xnahdmfh5TVRob3AzwTA`
 const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+// const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setloading] = useState(true)
+  const [photos, setPhotos] = useState([])
+
+  const fetchImages = async () => {
+    setloading(true)
+    try {
+      const resp = await fetch(`${mainUrl}${clientID}`)
+      const data = await resp.json()
+      setPhotos(data)
+      setloading(false)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+      setloading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchImages()
+  }, [])
+  if (loading) return <div>Loading...</div>
+
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form'>
+          <input
+            type='text'
+            placeholder='search'
+            className='form-input'
+            // value=''
+          />
+          <button type='submit' className='submit-btn'>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((item) => {
+            return <Photo key={item.id} {...item} />
+          })}
+        </div>
+        {loading && <h2 className='loading'>Loading...</h2>}
+      </section>
+    </main>
+  )
 }
 
 export default App
--- 17-stock-photos/src/Photo.js
+++ 17-stock-photos/src/Photo.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
+const Photo = ({
+  urls: { regular },
+  alt_description,
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+}) => {
+  return (
+    <article className='photo'>
+      <img src={regular} alt={alt_description} />
+      <div className='photo-info'>
+        <div>
+          <h4>{name}</h4>
+          <p>{likes}</p>
+        </div>
+        <a href={portfolio_url}>
+          <img src={medium} alt={name} className='user-img' />
+        </a>
+      </div>
+    </article>
+  )
 }
 
 export default Photo
